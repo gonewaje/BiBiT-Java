0367fb91f31eb6edd0084f3bdebd36627c0d1166
gonewaje <gone.waje1@gmail.com>
2024-09-28 21:52:57 +0700

    add java app

---
+++ src/main/java/com/example/HelloWorldWebAppApplication.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class HelloWorldWebAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldWebAppApplication.class, args);
+    }
+}
+
+@RestController
+class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "<div style='text-align: center;'>" +
+               "<h1>Hello World V2.0!</h1>" +
+               "<h2>DEMO SPRING MAVEN BSI</h2>" +
+               "</div>";
+    }
+}
+
